3392a34a483ae19b4c328af90428c7ebab6d71a5
Joel Coutinho <joelsteve97@hotmail.com>
2023-09-07 17:08:22 +0530

    fixs

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
-const { Client }  = require("discord.js")
+const { Client } = require("discord.js")
 require("dotenv").config()
 
 const BOT_TOKEN = process.env.BOT_TOKEN
 
-const bot = new Client({ intents: ["Guilds", "GuildMessages","MessageContent", "GuildMembers"] })
+const bot = new Client({ intents: ["Guilds", "GuildMessages", "MessageContent", "GuildMembers"] })
 bot.login(BOT_TOKEN)
 
 bot.on("ready", () => {
@@ -22,26 +22,36 @@ function generateIntroductionMessage(memberId) {
 
 bot.on('guildMemberAdd', async member => {
     const channel = await bot.channels.fetch(INTRODUCIONS_CHANNEL_ID)
-    channel.send({content: generateIntroductionMessage(member.id)})
+    channel.send({ content: generateIntroductionMessage(member.id) })
 })
+
+let i = 0;
 
 bot.on("messageCreate", async msg => {
 
-    if (msg.channel.id.toString() == INTRODUCIONS_CHANNEL_ID){
+    if (msg.channel.id.toString() == INTRODUCIONS_CHANNEL_ID) {
 
         const member = msg.guild.members.cache.get(msg.author.id)
 
+        // Ignore bots
+        if (member.user.bot) {
+            return
+        }
+
+        // Ignore messages from members who already have access to the rest of the server
         if (member.roles.cache.has(MEMBER_ROLE_ID)) {
             return msg.reply(`Hey <@${member.id}>, you already have access to the rest of the server. Please head to the #general channel to chat with the community.`)
         }
 
+        // Ignore messages that don't start with !intro
         if (!msg.content.startsWith("!intro")) {
             return msg.reply(`Your introduction must start with !intro and must be longer than 20 characters.`)
         }
 
+        // Ignore messages that are too short
         const message = msg.content.split("!intro")[1]
         const minMsgLength = 20
-        if(message.length < minMsgLength) {
+        if (message.length < minMsgLength) {
             return msg.reply(`Please introduce yourself with a message longer than ${minMsgLength} characters.`)
         }
 
@@ -50,7 +60,7 @@ bot.on("messageCreate", async msg => {
             await member.roles.add(memberRole)
             return msg.reply(`Successfully introduced yourself! You now have access to the rest of the server. If you have any questions, please head to the <#${SUPPORT_CHANNEL_ID}> channel to ask questions.`)
 
-        } catch (error){
+        } catch (error) {
             console.log("Error: ", error)
         }
     }
